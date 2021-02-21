b4a33dbcb2430c880e118bb0c51b1bab365d8bd0
Mikel-Jon West <dwmyke@gmail.com>
2021-02-20 23:44:34 -0800

    reinstalled sanity

--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -87,7 +87,8 @@ const IndexPage = props => {
     <Layout>
       <SEO title={site.title} description={site.description} keywords={site.keywords} />
       <Container>
-        <h1 hidden>Welcome to {site.title}</h1>
+        <h1>Welcome to {site.title}</h1>
+        <p>{site.subtitle}</p>
         {projectNodes && (
           <ProjectPreviewGrid
             title='Latest projects'

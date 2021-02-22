71637fa8f0e3041aefeb2eb01ca99f5ef02ae34e
Mikel-Jon West <dwmyke@gmail.com>
2021-02-21 22:16:14 -0800

    finished submission created function. Didn't know if the gatsby config token will work. Testing.

--- web/functions/submission-created.js
+++ web/functions/submission-created.js
@@ -1,8 +1,18 @@
 exports.handler = async function (event, context, callback) {
     // Pulling out the payload from the body
     const { payload } = JSON.parse(event.body)
-    console.log(payload)
-    
+    // Checking which form has been submitted
+    const isContactForm = payload.data.formId === "contact-form"
+    // Build the document JSON and submit it to SANITY
+    if (isContactForm) {
+      const contact = {
+        _type: "contact",
+        name: payload.data.name,
+        email: payload.data.email,
+        message: payload.data.message,
+      }
+      const result = await client.create(contact).catch((err) => console.log(err))
+    }
     callback(null, {
       statusCode: 200,
     })
